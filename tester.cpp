391bcab01d9a5be74ae0af67bb43f6b28a493bf5
Witnessvan <32366597+witnessivan@users.noreply.github.com>
2018-02-19 14:47:28 +0300

    Update tester.cpp

--- tester.cpp
+++ tester.cpp
@@ -1,43 +1,57 @@
 #include"Calculator.h"
 int main() {
 	double a, b, c, d, n, t, f;
-	cout << "Enter the numbers : a, b - this is x; c, d is y, n is a prime number(if you want to multiply a vector by a prime number, then enter b, d = 0)" << endl;
-	cin >> a >> b >> c >> d >> n;
-	cout << "Choose operation: +, -, *,$" << endl;
+	cout << "Enter first vector " << endl;
+	cout << "Enter x1 =;y1=" << endl;
+	cin >> a >> c;
+	cout << "Enter second vector" << endl;
+	cout << "Enter x2 =;y2=" << endl;
+	cin >> b >> d;
+	cout << "Enter n=";
+	cin >> n;
+	cout << "Choose operation: +, -, *-(multiply number),#-(scalar operation)" << endl;
 	char op = ' ';
 	cin >> op;
-	switch(op) {
-	      case'+': {
-			  t = addx(a, b);
-			  f = addy(c, d);
+	switch (op) {
+	case'+': {
+		t = addx(a, b);
+		f = addy(c, d);
+		printresult(t);
+		printresult(f);
 
-	      }
-				   break;
-		  case'-': {
-			  t = substractx(a, b);
-			  f = substracty(c, d);
-
-		  }
-				   break;
-		  case'*': {
-			  t = multiplyx(a, n);
-			  f = multiplyy(c, n);
-
-		  }
-				   break;
-		  case'$': {
-			  t = stray(a, b, c, d);
-		  }
-				   break;
-
-		  default: {
-			  cout << "ERROR";
-
-		  }
-				   break;
 	}
-	printresult(t);
-	printresult(f);
-	system("pause");
+			 break;
+	case'-': {
+		t = substractx(a, b);
+		f = substracty(c, d);
+		printresult(t);
+		printresult(f);
+
+	}
+			 break;
+	case'*': {
+		t = multiplyx(a, n);
+		f = multiplyy(c, n);
+		printresult(t);
+		printresult(f);
+
+	}
+			 break;
+	case'#': {
+		t = stray(a, b, c, d);
+		printresult(t);
+		
+	}
+			 break;
+
+	default: {
+		cout << "ERROR"<<op<<"-This character is not supported";
+
+	}
+			 break;
+	}
 	
+	system("pause");
+	return 0;
+
 }
